dfbc92d0eb51ed2e56c7c3a433868a6ff333cee2
Lauro Figueiredo <lauro.figueiredo@avenuecode.com>
2022-02-05 23:49:15 -0300

    feat: allow subdirectories in posts dir

--- .vitepress/posts.data.js
+++ .vitepress/posts.data.js
@@ -1,59 +1,74 @@
-const fs = require('fs')
-const path = require('path')
-const matter = require('gray-matter')
-const { createMarkdownRenderer } = require('vitepress')
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+const { createMarkdownRenderer } = require('vitepress');
 
-const md = createMarkdownRenderer(process.cwd())
+const md = createMarkdownRenderer(process.cwd());
 
-module.exports = {
-  watch: '../posts/*.md',
-  load(asFeed = false) {
-    const postDir = path.resolve(__dirname, '../posts')
-    return fs
-      .readdirSync(postDir)
-      .map((file) => getPost(file, postDir, asFeed))
-      .sort((a, b) => b.date.time - a.date.time)
+function* walkSync(dir) {
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* walkSync(path.join(dir, file.name));
+    } else {
+      yield path.join(dir, file.name);
+    }
   }
 }
 
-const cache = new Map()
+module.exports = {
+  watch: '../posts/**/*.md',
+  load(asFeed = false) {
+    const postDir = path.resolve(__dirname, '../posts');
+    const posts = [];
 
-function getPost(file, postDir, asFeed = false) {
-  const fullePath = path.join(postDir, file)
-  const timestamp = fs.statSync(fullePath).mtimeMs
+    for (const path of walkSync(postDir)) {
+      const post = getPost(path, asFeed);
+      posts.push(post);
+    }
 
-  const cached = cache.get(fullePath)
+    return posts
+      .sort((a, b) => b.date.time - a.date.time);
+  }
+};
+
+const cache = new Map();
+
+function getPost(fullPath, asFeed = false) {
+  const timestamp = fs.statSync(fullPath).mtimeMs;
+
+  const cached = cache.get(fullPath);
   if (cached && timestamp === cached.timestamp) {
-    return cached.post
+    return cached.post;
   }
 
-  const src = fs.readFileSync(fullePath, 'utf-8')
-  const { data, excerpt } = matter(src, { excerpt: true })
+  const src = fs.readFileSync(fullPath, 'utf-8');
+  const { data, excerpt } = matter(src, { excerpt: true });
 
   const post = {
     title: data.title,
-    href: `/posts/${file.replace(/\.md$/, '.html')}`,
+    href: fullPath.replace(/.+?(?=\/posts)/, '').replace(/\.md$/, '.html'),
     date: formatDate(data.date),
     excerpt: md.render(excerpt)
-  }
+  };
   if (asFeed) {
     // only attach these when building the RSS feed to avoid bloating the
     // client bundle size
-    post.data = data
+    post.data = data;
   }
 
-  cache.set(fullePath, {
+  cache.set(fullPath, {
     timestamp,
     post
-  })
-  return post
+  });
+  return post;
 }
 
 function formatDate(date) {
   if (!(date instanceof Date)) {
-    date = new Date(date)
+    date = new Date(date);
   }
-  date.setUTCHours(12)
+  date.setUTCHours(12);
   return {
     time: +date,
     string: date.toLocaleDateString('en-US', {
@@ -61,5 +76,5 @@ function formatDate(date) {
       month: 'long',
       day: 'numeric'
     })
-  }
+  };
 }
